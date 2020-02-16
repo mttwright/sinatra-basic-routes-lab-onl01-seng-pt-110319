09d1bc23b12cecb03e5f90a073bac2018efd75ab
Matt Wright <mttwright@gmail.com>
2020-02-16 21:02:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Yerrrrrr!"
+  end
+  
+  get '/hometown' do
+    "My hometown is Whoaaaaa!"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is yeeeeeeet!"
+  end
+  
+  
+  
 end
